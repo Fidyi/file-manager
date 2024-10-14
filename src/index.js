daff380401766db4a3befe0a1a4e9e1bde2af3ce
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-10-14 16:39:40 +0300

    feat: add Invalid input message

--- src/index.js
+++ src/index.js
@@ -15,13 +15,31 @@ const promptUser = () => {
     process.stdout.write('Enter command: ');
 };
 
+const handleCommand = (input) => {
+    const [command, ...params] = input.split(' ');
+
+    switch (command) {
+        case 'up':
+            // need to realize
+            break;
+        case 'cd':
+            // need to realize
+            break;
+        case 'ls':
+            // need to realize
+            break;
+        case '.exit':
+            console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+            process.exit(0);
+        default:
+            console.log('Invalid input: Unknown command. Please try again.'); // Сообщение об ошибке
+            break;
+    }
+};
+
 process.stdin.on('data', (data) => {
     const input = data.toString().trim();
-
-    if (input === '.exit') {
-        console.log(`Thank you for using File Manager, ${username}, goodbye!`);
-        process.exit(0);
-    }
+    handleCommand(input);
     printCurrentDirectory();
     promptUser();
 });
@@ -30,4 +48,5 @@ process.on('SIGINT', () => {
     console.log(`\nThank you for using File Manager, ${username}, goodbye!`);
     process.exit(0);
 });
+
 promptUser();
