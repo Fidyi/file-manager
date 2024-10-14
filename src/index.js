53dfb8b917bfffbad92db0350966a142c2145f7d
Sergei <106452081+Fidyi@users.noreply.github.com>
2024-10-14 16:22:29 +0300

    feat: add messege current directory

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import os from 'os';
 const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--username='));
 const username = usernameArg ? usernameArg.split('=')[1] : 'user';
-
+const printCurrentDirectory = () => {
+    console.log(`You are currently in ${process.cwd()}`);
+};
 console.log(`Welcome to the File Manager, ${username}!`);
-
+printCurrentDirectory();
 process.stdin.on('data', (data) => {
     const input = data.toString().trim();
 
@@ -13,6 +15,7 @@ process.stdin.on('data', (data) => {
         console.log(`Thank you for using File Manager, ${username}, goodbye!`);
         process.exit(0);
     }
+    printCurrentDirectory();
 });
 
 process.on('SIGINT', () => {
